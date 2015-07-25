506049d68de7e880797ba3c17246bc8187dfab97
Shen Chauhan <shenchauhan@hotmail.com>
2015-07-25 04:06:18 -0700

    Adding buttons

--- SplitViewOptions/SplitViewOptions/MainPage.xaml.cs
+++ SplitViewOptions/SplitViewOptions/MainPage.xaml.cs
@@ -31,46 +31,46 @@ namespace SplitViewOptions
         {
             base.OnNavigatedTo(e);
 
-            this.PaneOpenButton.Content = this.MySplitView.IsPaneOpen ? "Open" : "Closed";
-            this.MySplitView.PaneClosed += MySplitView_PaneClosed;
+            //this.PaneOpenButton.Content = this.MySplitView.IsPaneOpen ? "Open" : "Closed";
+            //this.MySplitView.PaneClosed += MySplitView_PaneClosed;
         }
 
-        private void MySplitView_PaneClosed(SplitView sender, object args)
-        {
-            this.PaneOpenButton.Content =  "Closed";
-        }
+        //private void MySplitView_PaneClosed(SplitView sender, object args)
+        //{
+        //    this.PaneOpenButton.Content =  "Closed";
+        //}
 
-        private void ChangeDisplayModeButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.MySplitView.DisplayMode == SplitViewDisplayMode.CompactInline)
-            {
-                this.MySplitView.DisplayMode = SplitViewDisplayMode.Inline;
-                this.ChangeDisplayModeButton.Content = "Inline";
-            }
-            else if (this.MySplitView.DisplayMode == SplitViewDisplayMode.Inline)
-            {
-                this.MySplitView.DisplayMode = SplitViewDisplayMode.CompactOverlay;
-                this.ChangeDisplayModeButton.Content = "CompactOverlay";
-            }
-            else if (this.MySplitView.DisplayMode == SplitViewDisplayMode.CompactOverlay)
-            {
-                this.MySplitView.DisplayMode = SplitViewDisplayMode.Overlay;
-                this.ChangeDisplayModeButton.Content = "Overlay";
-            }
-            else if (this.MySplitView.DisplayMode == SplitViewDisplayMode.Overlay)
-            {
-                this.MySplitView.DisplayMode = SplitViewDisplayMode.CompactInline;
-                this.ChangeDisplayModeButton.Content = "CompactInline";
-            }
+        //private void ChangeDisplayModeButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (this.MySplitView.DisplayMode == SplitViewDisplayMode.CompactInline)
+        //    {
+        //        this.MySplitView.DisplayMode = SplitViewDisplayMode.Inline;
+        //        this.ChangeDisplayModeButton.Content = "Inline";
+        //    }
+        //    else if (this.MySplitView.DisplayMode == SplitViewDisplayMode.Inline)
+        //    {
+        //        this.MySplitView.DisplayMode = SplitViewDisplayMode.CompactOverlay;
+        //        this.ChangeDisplayModeButton.Content = "CompactOverlay";
+        //    }
+        //    else if (this.MySplitView.DisplayMode == SplitViewDisplayMode.CompactOverlay)
+        //    {
+        //        this.MySplitView.DisplayMode = SplitViewDisplayMode.Overlay;
+        //        this.ChangeDisplayModeButton.Content = "Overlay";
+        //    }
+        //    else if (this.MySplitView.DisplayMode == SplitViewDisplayMode.Overlay)
+        //    {
+        //        this.MySplitView.DisplayMode = SplitViewDisplayMode.CompactInline;
+        //        this.ChangeDisplayModeButton.Content = "CompactInline";
+        //    }
 
-            this.MySplitView.IsPaneOpen = this.MySplitView.IsPaneOpen ? false : true;
-            this.PaneOpenButton.Content = this.MySplitView.IsPaneOpen ? "Open" : "Closed";
-        }
+        //    this.MySplitView.IsPaneOpen = this.MySplitView.IsPaneOpen ? false : true;
+        //    this.PaneOpenButton.Content = this.MySplitView.IsPaneOpen ? "Open" : "Closed";
+        //}
 
-        private void PaneOpenButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.MySplitView.IsPaneOpen = this.MySplitView.IsPaneOpen ? false : true;
-            this.PaneOpenButton.Content = this.MySplitView.IsPaneOpen ? "Open" : "Closed";
-        }
+        //private void PaneOpenButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.MySplitView.IsPaneOpen = this.MySplitView.IsPaneOpen ? false : true;
+        //    this.PaneOpenButton.Content = this.MySplitView.IsPaneOpen ? "Open" : "Closed";
+        //}
     }
 }
