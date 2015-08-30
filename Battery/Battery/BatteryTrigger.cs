67cfebf3a6a98e0eea34f4ef8332a94ad1c9bf0b
shenchauhan <shenchauhan@hotmail.com>
2015-08-29 20:16:49 -0700

    Battery application

---
+++ Battery/Battery/BatteryTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.System.Power;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace Battery
+{
+	public class BatteryTrigger : StateTriggerBase
+	{
+		public bool Charging { get; set; }
+
+		public BatteryTrigger()
+		{
+			Windows.Devices.Power.Battery.AggregateBattery.ReportUpdated 
+				+= async (sender, args) => await UpdateStatus();
+
+			UpdateStatus();
+		}
+		
+
+			private async Task UpdateStatus()
+			{
+				await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+				{
+					var batteryReport = Windows.Devices.Power.Battery.AggregateBattery.GetReport();
+					var percentage = (batteryReport.RemainingCapacityInMilliwattHours.Value /
+									  (double)batteryReport.FullChargeCapacityInMilliwattHours.Value);
+
+					if (percentage < 0.85)
+					{
+						switch (batteryReport.Status)
+						{
+							case BatteryStatus.Charging:
+								SetActive(!this.Charging);
+								break;
+							case BatteryStatus.Discharging:
+								SetActive(this.Charging);
+								break;
+						}
+					}
+					else
+					{
+						SetActive(!this.Charging);
+					}
+				});
+			}
+	}
+}
---
+++ Battery/Battery/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Battery
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		protected async override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+			Windows.Devices.Power.Battery.AggregateBattery.ReportUpdated += AggregateBatteryOnReportUpdated;
+
+			await UpdatePercentage(Windows.Devices.Power.Battery.AggregateBattery);
+		}
+
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+			base.OnNavigatedFrom(e);
+			Windows.Devices.Power.Battery.AggregateBattery.ReportUpdated -= AggregateBatteryOnReportUpdated;
+		}
+
+		private async void AggregateBatteryOnReportUpdated(Windows.Devices.Power.Battery sender, object args)
+		{
+			await UpdatePercentage(sender);
+		}
+
+		private async Task UpdatePercentage(Windows.Devices.Power.Battery sender)
+		{
+			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			{
+				var batteryReport = sender.GetReport();
+				var percentage = (batteryReport.RemainingCapacityInMilliwattHours.Value /
+								  (double)batteryReport.FullChargeCapacityInMilliwattHours.Value);
+				this.BatteryPercentageTextBlock.Text = percentage.ToString("##%");
+			});
+		}
+	}
+}
